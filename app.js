a702c38b3871e8062bff0eb549e40c00257c0bb4
majidamini <majidamini_s@yahoo.com>
2023-06-14 14:37:55 +0330

    footer section added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
-  loop: true,
-  
+  // slidesPerView: 3,
+  spaceBetween: 30,
+
   // If we need pagination
   // pagination: {
   //   el: '.swiper-pagination',
@@ -12,6 +13,43 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+const mySwiper = new Swiper(".mySwiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 2,
+  spaceBetween: 20,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+     
+    },
+    // when window width is >= 480px
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
   },
 
   // And if we need scrollbar
