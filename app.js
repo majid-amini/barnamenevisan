5e051b3df5ec0e1b7a2c09bae42cfda795aadecb
majidamini <majidamini_s@yahoo.com>
2023-06-17 14:04:47 +0330

    our team section added

--- app.js
+++ app.js
@@ -29,6 +29,41 @@ const mySwiper = new Swiper(".mySwiper", {
   autoplay: {
     delay: 2000,
   },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next.next-2",
+    prevEl: ".swiper-button-prev.prev-2",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 480px
+
+    // when window width is >= 640px
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+const swiperPortfolio = new Swiper(".swiperPortfolio", {
+  // Optional parameters
+  direction: "horizontal",
+  spaceBetween: 20,
+  // slidesPerView: 4,
+  autoplay: true,
+  autoplay: {
+    delay: 2500,
+  },
   // If we need pagination
   // pagination: {
   //   el: '.swiper-pagination',
@@ -42,12 +77,13 @@ const mySwiper = new Swiper(".mySwiper", {
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 1,
-      
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
     },
     1024: {
-      slidesPerView: 2,
-      spaceBetween: 30,
+      slidesPerView: 4,
     },
     // when window width is >= 480px
 
