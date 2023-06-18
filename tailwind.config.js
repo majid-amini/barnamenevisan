284eae8c516fd074246cc1e7a0f1b75fecc6f2f3
majidamini <majidamini_s@yahoo.com>
2023-06-18 16:44:58 +0330

    form border changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,30 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    colors: {
-      primaryColor: {
-        100: "#dbeafe",
-        200: "#bfdbfe",
-        300: "#93c5fd",
-        400: "#60a5fa",
-        500: "#3b82f6",
+    extend: {
+      colors: {
+        primaryColor: {
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+        },
+        secondaryColor: {
+          100: "#ede9fe",
+          200: "#ddd6fe",
+          300: "#c4b5fd",
+          400: "#a78bfa",
+          500: "#8b5cf6",
+        },
+        lightBlueColor: "#93c5fd",
+        darkBlueColor: "#1d4ed8",
+        whiteColor: "#fff",
+        darkColor: "#2d2d2d",
+        lightGrayColor: "#eee",
+        darkGrayColor: "#6b7280",
       },
-      secondaryColor: {
-        100: "#ede9fe",
-        200: "#ddd6fe",
-        300: "#c4b5fd",
-        400: "#a78bfa",
-        500: "#8b5cf6",
-      },
-      lightBlueColor: "#93c5fd",
-      darkBlueColor: "#1d4ed8",
-      whiteColor: "#fff",
-      darkColor: "#2d2d2d",
-      lightGrayColor: "#eee",
-      darkGrayColor: "#6b7280",
     },
-    extend: {},
   },
   plugins: [],
 };
